fa33bd6c96cd313fa46bb9501dee571c8824f53c
kylix <2738386186@qq.com>
2024-11-27 11:53:59 +0800

    test_result.py文件路径修改

--- test_result.py
+++ test_result.py
@@ -3,7 +3,7 @@ import json
 
 class Test_simple():
     def test_card(self):
-        json_file = 'lab1/eval/results.json'
+        json_file = 'eval/results.json'
         with open(json_file, 'r') as f:
             data = json.load(f)
         act = data.get('act')
